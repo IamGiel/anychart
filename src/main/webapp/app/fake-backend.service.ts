8fd4b313c0e9aec3cc4a3fd325a631ceb97adcc3
Ranjan <ranjan.patra@aricent.com>
2019-05-17 16:08:29 +0530

    bux fixes

--- src/main/webapp/app/fake-backend.service.ts
+++ src/main/webapp/app/fake-backend.service.ts
@@ -1,6 +1,6 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
-import * as sharedRequest from '../app/mock-data/sharedRequest.json';
-import * as pendingRequest from '../app/mock-data/pendingRequest.json';
+/*import  * as sharedRequest from '../app/mock-data/sharedRequest.json';
+import * as pendingRequest from '../mock-data/pendingRequest.json';
 import * as declinedRequest from '../app/mock-data/declinedRequest.json';
 import * as CSRHUB from '../app/mock-data/CSRHUB.json';
 import * as DnB from '../app/mock-data/DnB.json';
@@ -10,8 +10,19 @@ import * as ratingTrendCode from '../app/mock-data/ratingTrendCode.json';
 import * as supplierDetailsbyId from '../app/mock-data/supplierDetailsbyId.json';
 import * as companyQuestionSection from '../app/mock-data/companyQuestionSection.json';
 import * as companyQuestion from '../app/mock-data/companyQuestion.json';
-import * as companyChildQuestion from '../app/mock-data/companyChildQuestion.json';
-
+import * as companyChildQuestion from '../app/mock-data/companyChildQuestion.json';*/
+var sharedRequest = require('../app/mock-data/sharedRequest.json');
+var pendingRequest = require('../app/mock-data/pendingRequest.json');
+var declinedRequest = require('../app/mock-data/declinedRequest.json');
+var CSRHUB = require('../app/mock-data/CSRHUB.json');
+var DnB = require('../app/mock-data/DnB.json');
+var DOW_JONES = require('../app/mock-data/DOW_JONES.json');
+var erpDetails = require('../app/mock-data/supplier-details.json');
+var ratingTrendCode = require('../app/mock-data/ratingTrendCode.json');
+var supplierDetailsbyId = require('../app/mock-data/supplierDetailsbyId.json');
+var companyQuestionSection = require('../app/mock-data/companyQuestionSection.json');
+var companyQuestion = require('../app/mock-data/companyQuestion.json');
+var companyChildQuestion = require('../app/mock-data/companyChildQuestion.json');
 export class FakeBackendService implements InMemoryDbService {
     createDb() {
         let demoJson = [
