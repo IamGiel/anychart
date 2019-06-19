36490bcc078178b76904908c96bc22dbd432170d
Macbook <macbook@Macbooks-MacBook-Pro.local>
2019-06-19 01:48:40 -0700

    tooltip color changes

--- src/main/webapp/app/entities/common/components/charts/column/column-chart.component.ts
+++ src/main/webapp/app/entities/common/components/charts/column/column-chart.component.ts
@@ -24,6 +24,79 @@ export class ColumnChartComponent implements OnInit, AfterViewInit {
     ngOnchanges() {
         console.log(this.chartTitle);
     }
+    // ngAfterViewInit() {
+    //     const series = this.chart.column(this.chartData);
+    //     this.chart.container(this.container.nativeElement);
+    //     series.fill('rgb(231, 231, 231)');
+    //     series.stroke('rgb(231, 231, 231)');
+    //     this.chart.yAxis().stroke('#ffff');
+    //     this.chart.yAxis().labels(false);
+    //     if (this.labels !== true) {
+    //         this.chart.xAxis().labels(false);
+    //     }
+    //     this.chart
+    //         .yAxis()
+    //         .ticks()
+    //         .stroke('#ffff');
+    //     this.chart
+    //         .xAxis()
+    //         .ticks()
+    //         .stroke('#ffff');
+    //     this.chart.xAxis().stroke('#ffff');
+    //     // this.chart.xAxis().labels(false);
+    //     this.chart
+    //         .xAxis()
+    //         .ticks()
+    //         .stroke('#ffff');
+    //     this.chart
+    //         .xAxis()
+    //         .ticks()
+    //         .stroke('#ffff');
+    //     // const title = this.chart.xAxis().title();
+    //     // title.text(this.chartTitle);
+    //     // title.fontWeight(400);
+    //     // title.enabled(true);
+    //     //this.chart.title().letterSpacing('1px');
+    //     // set font family
+    //     this.chart.title().fontFamily('Inter UI');
+    //     // get chart tooltip
+    //     this.chart.tooltip().useHtml(true);
+    //     var tooltip = this.chart.tooltip();
+    //     tooltip.title(false);
+    //     tooltip.separator(false);
+    //     tooltip.fontFamily('Inter UI');
+    //     tooltip.format('<b>{%x}</b>: <b>{%value}</b>');
+    //     //this.chart.labels().position('outside');
+    //     // disable the legend
+    //     //this.chart.legend(false);
+    //     if (this.hideTooltipPerct != 'true') {
+    //         this.chart.labels().position('outside');
+    //     }
+    //     if (this.hideTooltipPerct == 'true') {
+    //         // set the position of the legend
+    //         this.chart.legend().itemsLayout('vertical');
+    //         this.chart.legend().position('right');
+    //         // set the alignment of the legend
+    //         this.chart.legend().align('center');
+    //     }
+    //     if (this.showValue == 'true') {
+    //         this.chart.labels().format('{%value}');
+    //     }
+    //     this.chart.title().fontFamily('Inter UI');
+    //     this.chart.tooltip().background().fill("#FFFFFF");
+    //     this.chart.tooltip().padding().left(20);
+    //     this.chart.tooltip().separator(false);
+    //     this.chart.tooltip().fontColor("#000000");
+    //     this.chart.legend().fontSize(9);
+    //     //this.chart.legend().maxHeight("30%");
+    //     // this.chart.legend().maxWidth("100%");
+    //     this.chart.normal().stroke(null);
+    //     // set the layout of the legend
+    //     // this.chart.legend().itemsLayout("horizontal-expandable")
+    //     this.chart.legend().fontFamily('Inter UI');
+    //     // this.chart.yAxis().title('No. of suppliers');
+    //     this.chart.draw();
+    // }
     ngAfterViewInit() {
         const series = this.chart.column(this.chartData);
         this.chart.container(this.container.nativeElement);
@@ -58,14 +131,33 @@ export class ColumnChartComponent implements OnInit, AfterViewInit {
         // title.enabled(true);
         //this.chart.title().letterSpacing('1px');
         // set font family
+
         this.chart.title().fontFamily('Inter UI');
+        this.chart
+            .tooltip()
+            .background()
+            .fill('#FFFFFF');
+        this.chart
+            .tooltip()
+            .padding()
+            .left(20);
+        this.chart.tooltip().separator(false);
+        this.chart.tooltip().fontColor('#000000');
+        // this.chart.tooltip().useHtml(true);
+        // this.chart.tooltip().padding().left(20);
+        // this.chart.tooltip().format('Rating 8');
+
+        //
+        // this.chart.tooltip().format('<b>{%x}</b>: <b>{%value}</b>');
+        // this.chart.tooltip().useHtml(true);
         // get chart tooltip
-        this.chart.tooltip().useHtml(true);
-        var tooltip = this.chart.tooltip();
-        tooltip.title(false);
-        tooltip.separator(false);
-        tooltip.fontFamily('Inter UI');
-        tooltip.format('<b>{%x}</b>: <b>{%value}</b>');
+        // this.chart.tooltip().useHtml(true);
+        // var tooltip = this.chart.tooltip();
+        // tooltip.title(false);
+        // tooltip.separator(false);
+        // tooltip.fontFamily('Inter UI');
+        //
+
         //this.chart.labels().position('outside');
         // disable the legend
         //this.chart.legend(false);
@@ -85,6 +177,7 @@ export class ColumnChartComponent implements OnInit, AfterViewInit {
         this.chart.legend().fontSize(9);
         //this.chart.legend().maxHeight("30%");
         // this.chart.legend().maxWidth("100%");
+        this.chart.legend().maxWidth('100%');
         this.chart.normal().stroke(null);
         // set the layout of the legend
         // this.chart.legend().itemsLayout("horizontal-expandable")
