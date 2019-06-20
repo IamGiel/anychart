a833736d3877421509072de042b4ee1ad58f74a0
Appiness <appiness@Appinesss-MacBook-Pro.local>
2019-06-20 16:06:06 +0530

    added carosoul and alignment changes

--- src/main/webapp/app/entities/procurement/supplierdata/supplierdata.component.ts
+++ src/main/webapp/app/entities/procurement/supplierdata/supplierdata.component.ts
@@ -31,6 +31,7 @@ export class SupplierdataComponent implements OnInit {
         document.addEventListener('click', this.offClickHandler.bind(this)); // bind on doc
     }
 
+    images = [1, 2, 3, 4, 5, 6].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
     responseFunnelData = [];
     chartData = [];
     financialdetails = 'More details';
