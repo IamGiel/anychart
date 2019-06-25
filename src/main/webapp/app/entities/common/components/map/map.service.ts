a20c7acad4e02a399342b08c899221cae2573755
pramodAIPL <pramod@appinessworld.com>
2019-06-25 18:32:40 +0530

    map data is coming from the .json file

--- src/main/webapp/app/entities/common/components/map/map.service.ts
+++ src/main/webapp/app/entities/common/components/map/map.service.ts
@@ -5,7 +5,12 @@ import { HttpClient } from '@angular/common/http';
     providedIn: 'root'
 })
 export class MapService {
-    constructor(private http: HttpClient) {}
+    mapData: any;
+    locationData: any;
+    constructor(private http: HttpClient) {
+        this.mapData = require('../../../../mock-data/mapEnvironmentPI.json');
+        this.locationData = require('../../../../mock-data/mapLocationsData.json');
+    }
 
     getCountrydata() {
         return this.http.get('src/main/webapp/app/mock-data/mapdata.json');
