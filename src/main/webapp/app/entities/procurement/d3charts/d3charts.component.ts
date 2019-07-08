8e19182e98f717d254074fde4bcce3080361583e
IamGiel <november14giel@gmail.com>
2019-07-08 13:58:38 -0400

    increment and pop

--- src/main/webapp/app/entities/procurement/d3charts/d3charts.component.ts
+++ src/main/webapp/app/entities/procurement/d3charts/d3charts.component.ts
@@ -14,6 +14,7 @@ export class D3chartsComponent implements OnInit {
     importJSON = dataJson;
     // data = `../d3charts/sample-data.json`;
     data: any[];
+    data2: any[];
     // data:any = 'https://unpkg.com/us-atlas@1.0.2/us/10m.json';
     @ViewChild('graphContainer') graphContainer: ElementRef;
 
@@ -24,6 +25,33 @@ export class D3chartsComponent implements OnInit {
         // console.log("test somedata here",this.someDataHere)
         // console.log("test d3 ",d3)
         this.data = [
+            {
+                name: 'Young',
+                order: 'Veggie Soup',
+                value: 700
+            },
+            {
+                name: 'James',
+                order: 'Baconator with Fries',
+                value: 100
+            },
+            {
+                name: 'Greg',
+                order: 'Chicken Parmesean',
+                value: 120
+            },
+            {
+                name: 'Bryan',
+                order: 'Steak and egg',
+                value: 180
+            },
+            {
+                name: 'Meg',
+                order: 'Milkshake',
+                value: 1800
+            }
+        ];
+        this.data2 = [
             {
                 name: 'Young',
                 order: 'Veggie Soup',
@@ -61,6 +89,7 @@ export class D3chartsComponent implements OnInit {
 
         // select svg container
         const svg = d3.select('#graphContainer');
+        const svg2 = d3.select('#graphContainer2');
 
         const margin = { top: 20, right: 20, bottom: 100, left: 100 };
         const graphWidth = 600 - margin.left - margin.right;
@@ -72,8 +101,16 @@ export class D3chartsComponent implements OnInit {
             .attr('height', graphHeight)
             .attr('transform', `translate( ${margin.left}, ${margin.right})`);
 
+        const graph2 = svg2
+            .append('g')
+            .attr('width', graphWidth)
+            .attr('height', graphHeight)
+            .attr('transform', `translate( ${margin.left}, ${margin.right})`);
+
         const xAxisGroup = graph.append('g').attr('transform', `translate(0, ${graphHeight})`);
         const yAxisGroup = graph.append('g');
+        const xAxisGroup2 = graph2.append('g').attr('transform', `translate(0, ${graphHeight})`);
+        const yAxisGroup2 = graph2.append('g');
 
         // scale the y axis
         const y = d3.scaleLinear().range([graphHeight, 0]);
@@ -96,7 +133,7 @@ export class D3chartsComponent implements OnInit {
         const t = d3.transition().duration(500);
 
         // =========== update data ===========
-        const update = data => {
+        const update1stGraph = data => {
             data = this.data;
             // update scales
             y.domain([0, d3.max(this.data, d => d.value)]);
@@ -135,12 +172,55 @@ export class D3chartsComponent implements OnInit {
                 .attr('text-anchor', 'end');
         };
 
+        const update2ndGraph = data => {
+            data = this.data2;
+            // update scales
+            y.domain([0, d3.max(this.data2, d => d.value)]);
+            x.domain(this.data2.map(item => item.name));
+
+            // join data
+            const rect = graph2.selectAll('rect').data(this.data2);
+            // remove entries when data is updated
+            rect.exit().remove();
+
+            rect
+                .attr('width', x.bandwidth)
+                .attr('fill', 'lightblue')
+                .attr('x', d => x(d.name));
+
+            rect
+                .enter()
+                .append('rect')
+                .attr('width', x.bandwidth)
+                .attr('height', 0)
+                .attr('y', graphHeight)
+                .attr('fill', 'lightblue')
+                .attr('x', d => x(d.name))
+                .merge(rect)
+                .transition(t)
+                .attr('height', d => graphHeight - y(d.value))
+                .attr('y', d => y(d.value));
+
+            // call axes
+            xAxisGroup2.call(xAxis);
+            yAxisGroup2.call(yAxis);
+
+            xAxisGroup2
+                .selectAll('text')
+                .attr('transform', 'rotate(-35)')
+                .attr('text-anchor', 'end');
+        };
+
         // change data every interval
         d3.interval(() => {
+            console.log((this.data[0].value += 50));
             this.data[0].value += 50;
-            update(this.data);
-        }, 1000);
+            this.data2.pop();
+            update1stGraph(this.data);
+            update2ndGraph(this.data2);
+        }, 3000);
 
-        update(this.data);
+        update1stGraph(this.data);
+        update2ndGraph(this.data2);
     }
 }
